e584afa0c2cf09d9f4bcc202a8500524a698be6e
dohlee <dohlee.bioinfo@gmail.com>
2020-12-18 21:14:27 +0900

    Bump version: 0.0.2 → 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'pyNetICS'
 year = '2020'
 author = 'BHI'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.2'
+version = release = '0.0.3'
 
 pygments_style = 'trac'
 templates_path = ['.']
