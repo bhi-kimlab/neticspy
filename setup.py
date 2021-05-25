f18ec8ab64a8768bcc60afaddf9d3cb9f0f258bb
dohlee <dohlee.bioinfo@gmail.com>
2021-05-25 10:15:46 +0900

    Bump version: 0.0.5 → 0.1.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='neticspy',
-    version='0.0.5',
+    version='0.1.0',
     license='MIT',
     description='Python implementation of NetICS. (Network-based Integration of Multi-omics data).',
     long_description='%s\n%s' % (
